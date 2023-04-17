409e19ec644e5303def9a85dcc90a1d04e6d4e9a
Gabriel D. Padua <gabriel.d.padua21@gmail.com>
2023-04-16 23:49:23 -0300

    feat: ✨ first request to site

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+def search_autos(url):
+    try:
+       response = requests.get(url)
+       if response.status_code == 200:
+           print(response.text)
+    except Exception as error:
+        print("Erro to search veicule")
+        print(error)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    url = os.getenv("BASE_URL")
+    search_autos(f'{url}automoveis')
